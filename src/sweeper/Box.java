152f8dc92eb5d777889915eb0d533d936ae88f58
aleksandrkosanin <kosyanin@yandex.ru>
2023-08-22 23:40:26 +0300

    Initial commit

---
+++ src/sweeper/Box.java
@@ -0,0 +1,28 @@
+package sweeper;
+
+public enum Box {
+    ZERO,
+    NUM1,
+    NUM2,
+    NUM3,
+    NUM4,
+    NUM5,
+    NUM6,
+    NUM7,
+    NUM8,
+    BOMB,
+    OPENED,
+    CLOSED,
+    FLAGGED,
+    BOMBED,
+    NOBOMB;
+
+    public Object image;
+    Box getNumberBox () {
+        return Box.values()[this.ordinal() + 1];
+    }
+
+    int getNumber () {
+        return this.ordinal();
+    }
+}
